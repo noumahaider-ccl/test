62c0aa1be6ddfdf6a11c9036dcce02cabb1c0737
Nouman Haider <nouman.haider@cybernetic-controls.com>
2025-02-06 23:16:38 +0500

    Modified time tracker py file for pipeline

--- app/time_tracker.py
+++ app/time_tracker.py
@@ -6,7 +6,7 @@ from app.utils import save_time_logs, load_time_logs
 from waitress import serve
 
 # Initialize the Flask application
-app = Flask(__name__)
+app = Flask(_name_)
 
 TIME_LOGS_FILE = "time_logs.json"  # Define the file to store time logs
 
@@ -125,10 +125,9 @@ def index():
     return jsonify({"message": "Welcome to the Time Tracker API. Use /clock_in/<user_id>, /clock_out/<user_id>, or /view_time_log/<user_id> to interact with the app."}), 200
 
 
-if __name__ == "__main__":
+if _name_ == "_main_":
     # Start the Flask app
     # app.run(host="0.0.0.0", port=5000)
     serve(app, host="0.0.0.0", port=5000)
     # serve(app, host="0.0.0.0", port=5001)
     # app.run(host="127.0.0.1", port=5001)
-
